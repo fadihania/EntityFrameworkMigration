69f0071f58910874fc5d9e7944d5e703d7fe22ce
Fadi Hania <fhania@gmail.com>
2023-03-27 05:45:08 +0300

    Create Entity/Model class with required fields in DB

---
+++ EntityFrameworkMigration/Entities/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkMigration.Entities;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Maker { get; set; }
+    public string Model { get; set; }
+    public int ProductionYear { get; set; }
+}
