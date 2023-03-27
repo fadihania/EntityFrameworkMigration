09f3a518d7bbef318089c238d2fd874d578a993a
Fadi Hania <fhania@gmail.com>
2023-03-27 05:41:35 +0300

    Configure DB Context class to providing connection string

--- EntityFrameworkMigration/CarsDbContext.cs
+++ EntityFrameworkMigration/CarsDbContext.cs
@@ -4,5 +4,8 @@ namespace EntityFrameworkMigration;
 
 public class CarsDbContext : DbContext
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=cars.db");
+    }
 }
