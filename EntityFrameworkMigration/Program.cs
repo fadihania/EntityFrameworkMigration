6b35f3faa9081bf6f1980f4a1b113e5a90289904
Fadi Hania <fhania@gmail.com>
2023-03-28 20:27:58 +0300

    Insert multiple records in Cars table and save changes

--- EntityFrameworkMigration/Program.cs
+++ EntityFrameworkMigration/Program.cs
@@ -4,11 +4,19 @@ using EntityFrameworkMigration.Entities;
 var db = new CarsDbContext();
 
 // Insert
-db.Cars.Add(new Car
+//db.Cars.Add(new Car
+//{
+//    Maker = "VW",
+//    Model = "Polo",
+//    ProductionYear = 2022
+//});
+//await db.SaveChangesAsync();
+
+db.Cars.AddRange(new Car[]
 {
-    Maker = "VW",
-    Model = "Polo",
-    ProductionYear = 2022
+    new Car { Maker = "Fiat", Model = "Punto", ProductionYear = 2021 },
+    new Car { Maker = "KIA", Model = "Sorento", ProductionYear = 2018 },
+    new Car { Maker = "Mercedes", Model = "S-Class", ProductionYear = 2023 },
 });
 await db.SaveChangesAsync();
 
