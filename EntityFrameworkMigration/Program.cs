f6d78c1ffbe5cb15339963a7d24026783a8803b0
Fadi Hania <fhania@gmail.com>
2023-03-28 20:18:53 +0300

    Query all cars in database using LINQ

--- EntityFrameworkMigration/Program.cs
+++ EntityFrameworkMigration/Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkMigration;
+
+var db = new CarsDbContext();
+
+
+// Query
+var allCars = db.Cars.ToList();
+foreach (var car in allCars)
+{
+    Console.WriteLine(car);
+}
